a33d83cec91856dac0eefd77f4528b84e9eddf76
coreyderosa <coreyderosa@gmail.com>
2018-02-22 21:57:49 -0700

    Getting input value and displaying that value

---
+++ Lesson17/7-CollectingValuesQuiz.js
@@ -0,0 +1,14 @@
+/*
+For this quiz, use jQuery's val method to make live changes to the 'Cool Articles' <h1>!
+
+The starter code below creates an event listener that will run any time the input changes.
+For more on events, check the instructor notes.
+*/
+
+
+$('#input').on('change', function () {
+  var val;
+  val = $('input[type=text]').val();
+  $('.articles').find('h1').text(val); // Your code goes here!
+
+});
