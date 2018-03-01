fa56b54d431a48796a9a7e060d582136e6563657
coreyderosa <coreyderosa@gmail.com>
2018-02-28 22:04:39 -0700

    Added html inputs and css styling

---
+++ PixelArtMaker/script.js
@@ -0,0 +1,7 @@
+var colorWellHexValue = document.getElementById("color-well-id").value;
+document.getElementById("color-well-hex-value-id").innerHTML = colorWellHexValue;
+
+function colorWellValue() {
+  var colorWellHexValue = document.getElementById("color-well-id").value;
+  document.getElementById("color-well-hex-value-id").innerHTML = colorWellHexValue;
+}
