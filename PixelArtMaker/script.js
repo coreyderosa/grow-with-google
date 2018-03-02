beb6641d7b9960d8702f3988be1348de3a8cc0ef
coreyderosa <coreyderosa@gmail.com>
2018-03-01 22:00:05 -0700

    added some styling and grid stuff

--- PixelArtMaker/script.js
+++ PixelArtMaker/script.js
@@ -1,7 +1,28 @@
 var colorWellHexValue = document.getElementById("color-well-id").value;
-document.getElementById("color-well-hex-value-id").innerHTML = colorWellHexValue;
+document.getElementById("color-value-id").innerHTML = colorWellHexValue;
 
 function colorWellValue() {
   var colorWellHexValue = document.getElementById("color-well-id").value;
-  document.getElementById("color-well-hex-value-id").innerHTML = colorWellHexValue;
+  document.getElementById("color-value-id").innerHTML = colorWellHexValue;
 }
+
+function maxLength(maxlength) {
+  this.value = this.Value.slice(0, this.maxlength);
+}
+
+$("#submit-id").click(function () {
+  var rows = $("#rows-id").val();
+  var cols = $("#cols-id").val();
+
+  // console.log("rows: " + rows);
+  // console.log("columns: " + cols);
+  for (var h = 0; h < rows; h++) {
+    console.log(h);
+    $("#grid-id table").append("<tr></tr>");
+    for (var w = 0; w < cols; w++) {
+      console.log(w);
+      $("#grid-id table tr").append("<td class='col-lines'></td>");
+    }
+  }
+  return;
+});
